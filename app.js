5a37d57a5bb0cb7d607d4731f16f5a4c2e29a0e4
uzerp2 <uzerp2apple@gmail.com>
2018-09-20 11:21:13 +0300

    // 2

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 // app.js
 
 var express = require('express');
-
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var port = 3000;
 
-var itemRouter = require('./src/routes/itemRoutes');
-
-var mongoose = require('mongoose');
-
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-
-
+// Mongoose connection with mongodb
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://uzerp2:AllIwanna123@ds263172.mlab.com:63172/mongodb1') 
+mongoose.connect('mongodb://uzerp2:AllIwanna123@ds263172.mlab.com:63172/mongodb1')
     .then(() => { // if all is ok we will be here
-      console.log('Connected to mongodb1');
+      console.log('Start');
     })
     .catch(err => { // if error we will be here
         console.error('App starting error:', err.stack);
         process.exit(1);
     });
 
+// Required application specific custom router module
+var itemRouter = require('./src/routes/itemRoutes');
 
-
+// Use middlewares to set view engine and post json data to the server
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.use('/items', itemRouter);
 
-app.listen(port, function () {
-  console.log('Server is running on port:', port);
-});
-
+// Define home route
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+// Start the server
+app.listen(port, function(){
+  console.log('Server is running on Port: ',port);
 });
--- src/models/Item.js
+++ src/models/Item.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// Define collection and schema for Items
 var Item = new Schema({
   item: {
     type: String
